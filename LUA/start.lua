93cdfd868d823e2cd840c1ccb8f0e74f6e9dd40e
Anirs <68027758+Anirs123@users.noreply.github.com>
2025-01-29 14:35:38 +0100

    Lua learning

---
+++ LUA/start.lua
@@ -0,0 +1,50 @@
+print("Hello World!")
+-- dwa typy zmiennych sa
+local nazwa = 123123
+nazwa2 = 1232131
+
+
+--and or not
+
+
+if (not a==1) then
+    print("a nie jest rowne 1")
+elseif
+    --if
+    print("a")
+else
+    print("aa")
+end
+
+
+--infinite loop
+while true do
+    print()
+end
+
+for i=1,10 do
+    print(":)")
+end
+
+for init,min/max, step do
+    --logika
+end
+-- i to indeks v to value
+arr = {"1", "2", "3"}
+for i,v in ipairs (arr) do
+    print(i,v)
+end
+-- ipairs zwraca indeks i value
+table = {}
+table['key'] = 'value'
+for k,v in pairs(table) do
+    print(k,v)
+end
+-- pairs zwraca key i value
+
+function nazwa (par1, par2)
+    --logika
+    
+end
+
+nazwa(1,2)
